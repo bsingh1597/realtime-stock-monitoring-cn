5bbf6865043ab401c2b2f3d3708a672df1203821
Bipul Singh <bipul392ssnl@gmail.com>
2023-04-04 19:56:18 -0400

    updating the price

--- stock-streaming-client/src/components/StockClient.js
+++ stock-streaming-client/src/components/StockClient.js
@@ -22,29 +22,30 @@ export default function StockClient() {
     const [currentSubsTkr, setCurrentSubsTkr] = useState([])
 
     //  const intialStocks = ['AAPL','AMZN', '']
-    const intialStocks = ['AMZN']
+    const intialStocks = ['AMZN','BINANCE:BTCUSDT']
 
     const subscribeTemplate = '{"type":"subscribe","symbol":"{0}"}'
 
     const columns = [{
         Header: 'Symbol',
-        accessor: 'name'
+        accessor: 'symbol'
     }, {
         Header: 'Company Name',
-        accessor: 'age'
+        accessor: 'companyName'
     }, {
         Header: 'Price',
-        accessor: 'abc'
+        accessor: 'price'
     }, {
         Header: 'P/L',
-        accessor: 'def'
+        accessor: 'pi'
     }]
 
     useEffect(() => {
         // setCurrentSubsTkr({currentSubsTkr :  \[...currentSubsTkr, intialStocks]})
         intialStocks.map((tkr) => {
-            console.log("tkrs"+tkr)
+            console.log("tkrs 1: "+tkr)
             setCurrentSubsTkr(...currentSubsTkr, tkr)
+            setRowData([...rowData, {symbol : tkr, companyName: tkr, price: "", pi: ""}])
 
         })
         wsClient.onopen = () => {
@@ -58,25 +59,45 @@ export default function StockClient() {
             )
         };
 
-    })
+    },[])
 
     wsClient.onmessage = (res) => {
         console.log('On message' + JSON.stringify(res.data));
         const response = JSON.parse(res.data)
-        // console.log('On message2' + response.type);
+        console.log("checking "+JSON.stringify(response.data))
+        if(response.data) {
+            console.log("it is in")
+            updateStockPrice(res.data)
+        }
+
     };
 
+    const updateStockPrice = (data) => {
+        console.log('On message 1' + JSON.parse(data));
+        const stockRes = JSON.parse(data)
+        console.log("First data in the array: "+ JSON.stringify(stockRes.data[0].s))
+        setRowData(rowData.map(item => {
+            console.log("Iteam checking is: "+item.symbol)
+            
+            if (item.symbol === stockRes.data[0].s) {
+              return {
+                ...item,
+                price: stockRes.data[0].p,
+              };
+            }
+            return item;
+          }));
+    }
 
     const subscribeTkr = () => {
         console.log("Subs tkr before " + currentSubsTkr);
         const tkr = enterTkr.current.value
-        console.log("Tkr " + tkr)
-        console.log("Susbcribe Msg " + format(subscribeTemplate, tkr))
         wsClient.send(
             format(subscribeTemplate, tkr)
         )
         setCurrentSubsTkr(...currentSubsTkr, tkr)
         console.log("Subs tkr after " + currentSubsTkr);
+        setRowData([...rowData, {symbol : tkr, companyName: tkr, price: "", pi: ""}])
     };
 
     return (
