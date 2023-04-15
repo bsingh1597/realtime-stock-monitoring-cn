ff93607acc71a3fbcfa50c3fe222b6fb01007e42
harishanand51 <harishanand51@gmail.com>
2023-04-15 15:39:39 -0400

    post signup page

---
+++ stock-streaming-client/src/components/PostSignUp.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/PostSignUp.css";
+
+export default function PostSignUp() {
+  return (
+    <div className="verify-body">
+      <h3>VERIFY ACCOUNT</h3>
+      <p>Please verify your account to login.</p>
+      <p>A verification link has been sent to the registered email address</p>
+      <br></br>
+      <a href="/login" className="red">
+        Login here
+      </a>
+    </div>
+  );
+}
