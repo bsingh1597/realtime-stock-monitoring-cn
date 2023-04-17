18ec3abf3f0fb1e0b6cdf0d5e6eab9374f3fdc5f
charAt83 <saaigude@gmail.com>
2023-04-16 22:03:41 -0400

    Added Stock Map Reverse constants

--- stock-streaming-client/src/common/StockNames.js
+++ stock-streaming-client/src/common/StockNames.js
@@ -30,6 +30,32 @@ export const SYMBOL_MAP = {
     "FORD MOTOR CO": "F"
 }
 
+export const SYMBOL_MAP_REVERSE = {
+    "AAPL": "Apple",
+    "AMZN": "Amazon",
+    "GOOG": "Google",
+    "MSFT": "Microsoft",
+    "IBM": "IBM",
+    "SONY": "Sony",
+    "BINANCE:BTCUSDT": "Bitcoin USD",
+    "TSLA": "TESLA",
+    "JPM": "JPMORGAN",
+    "AMD": "AMD",
+    "NFLX": "NETFLIX",
+    "C": "CITIGROUP",
+    "BAC": "BANK OF AMERICA CORP",
+    "NVDA": "NVIDIA CORP",
+    "COST.L": "COSTAIN GROUP PLC",
+    "GS": "GOLDMAN SACHS GROUP INC",
+    "CRM": "SALESFORCE INC",
+    "INTC": "INTEL CORP",
+    "ZM": "ZOOM VIDEO COMMUNICATIONS",
+    "DIS": "WALT DISNEY CO",
+    "META": "META PLATFORMS",
+    "T": "AT & T",
+    "F": "FORD MOTOR CO"
+}
+
 export const FINHUB_TOKEN = "cgf7cgpr01qllg2ta1qgcgf7cgpr01qllg2ta1r0"
 
 export const FINHUB_WS_API = "wss://ws.finnhub.io?token="
